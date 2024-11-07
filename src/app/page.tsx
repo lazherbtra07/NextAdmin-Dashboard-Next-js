ac5040e78332da2af89db394c27c2b47d4bcbe02
mac <mac@macs-MacBook-Pro-2.local>
2024-11-07 13:55:20 +0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import ECommerce from "@/components/Dashboard/E-commerce";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLaout";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "NextAdmin Dashboard",
+  description: "This is Next.js Home page for NextAdmin Dashboard ",
+};
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
+  );
+}
